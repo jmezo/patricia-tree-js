8d009e9c958da1d8478b32a6a10929f533c0d8a9
Janos Mezo <jmezo.asd@gmail.com>
2021-11-15 19:17:03 +0100

    query block num in main fn

--- src/app.ts
+++ src/app.ts
@@ -6,23 +6,8 @@ import env from "./utils/env";
 const main = async () => {
   const db = levelup(leveldown(env.LEVELDB_PATH));
 
-  const blockHeader = await dbReader.getBlockHeader(db);
-
-  console.log(blockHeader);
-  console.log(await dbReader.getRootHashes(db));
-
-  const lastHeaderHash = await db.get(Buffer.from("LastHeader"));
-  console.log(lastHeaderHash);
-  const lastBlockHash = await db.get(Buffer.from("LastBlock"));
-  console.log(lastBlockHash);
-
-  const address = "069a9208A1DAde499F7609F7d52D815B1E60b95c";
-
-  const addrData = await dbReader.getAccount(address, db);
-  console.log(addrData);
-
-  const latestBlockNum = await dbReader.getLatestBlockNumber(db);
-  console.log(latestBlockNum);
+  const blockNumber = await dbReader.getLatestBlockNumber(db);
+  console.log(blockNumber);
 };
 
 main();
